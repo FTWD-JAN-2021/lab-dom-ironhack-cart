ecb23e13795a11c08270c86f5d23a45079086478
James Post <avilapost93@gmail.com>
2021-01-15 13:16:22 -0800

    james

--- js/index.js
+++ js/index.js
@@ -1,42 +1,12 @@
-// ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+document.querySelector('#calculate').onclick = function() {
+    console.log('calculating')
+    let allProducts = document.querySelectorAll('.product')
+    for (let i = 0 ; i < allProducts.length; i++){
+        let eachProduct = allProducts[i]
+            console.log(eachProduct)
+            let price = eachProduct.querySelector('.price span').innerHTML  
+            console.log(price)
+            
+    }
 }
-
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
-}
-
-// ITERATION 4
-
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
-
-// ITERATION 5
-
-function createProduct() {
-  //... your code goes here
-}
-
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
-});
