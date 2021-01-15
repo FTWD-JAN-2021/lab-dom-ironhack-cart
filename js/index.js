8a6ca6d6b679e21cdc4c4d103d6018082c0856bb
Xavier Bergeron <xavbergeron@gmail.com>
2021-01-15 14:13:17 -0800

    Create Product

--- js/index.js
+++ js/index.js
@@ -21,9 +21,41 @@ document.querySelector('#calculate').onclick = function() {
 document.querySelector('#create.btn').onclick = function(){
     console.log("I clicked the button")
 
-   let newProductName =  document.querySelector("#cart > tfoot > tr > td:nth-child(1) > input[type=text]").value
+    let newProductName =  document.querySelector("#cart > tfoot > tr > td:nth-child(1) > input[type=text]").value
 
-   let newProductPrice = document.querySelector('#cart > tfoot > tr > td:nth-child(2) > input[type=number]').value
+    let newProductPrice = document.querySelector('#cart > tfoot > tr > td:nth-child(2) > input[type=number]').value
 
-   console.log(newProductName,newProductPrice)
+    console.log(newProductName,newProductPrice)
+
+    // let productTemplate = document.querySelector('.product').outerHTML
+    // let tbody = document.querySelector("#cart tbody")
+
+    // tbody.innerHTML += productTemplate
+
+    // let newProduct = document.querySelector('.product:last-of-type')
+    // newProduct.querySelector('.name').innerHTML = newProductName
+    // newProduct.querySelector('.price span').innerHTML = newProductPrice
+    // newProduct.querySelector('.subtotal span').innerHTML = '0'
+    
+    let productRowHTML = `
+    <tr class="product">
+        <td class="name">
+            <span>${newProductName}</span>
+        </td>
+
+        <td class="price">$<span>${newProductPrice}</span></td>
+
+        <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+        </td>
+
+        <td class="subtotal">$<span>0</span></td>
+
+        <td class="action">
+            <button class="btn btn-remove">Remove</button>
+        </td>
+    </tr>
+    `
+    document.querySelector("#cart tbody").innerHTML += productRowHTML 
+
 }
